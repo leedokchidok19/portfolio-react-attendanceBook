837d337dc58332349c58eb552c5371a5b5300c61
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-07-19 01:50:54 +0900

    init:setting

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import Attendance from './components/attendance';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Attendance />
         </p>
         <a
           className="App-link"
---
+++ src/components/attendance.jsx
@@ -0,0 +1,12 @@
+//import './app.css';
+
+function AattendanceBook() {
+
+  return (
+    <div className="attendance">
+      <input type="text" className="attendance" placeholder="Please enter your name" />
+    </div>
+  );
+}
+
+export default AattendanceBook;
